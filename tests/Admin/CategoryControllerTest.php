0b892f0e72d9bf4473d5d0f6b940419b1bf58535
Geoffrey Vercaemer <geoffrey.vercaemer@gmail.com>
2022-02-04 16:53:31 +0100

    Fix Tests

--- tests/Admin/CategoryControllerTest.php
+++ tests/Admin/CategoryControllerTest.php
@@ -11,15 +11,4 @@ class CategoryControllerTest extends WebTestCase
     {
         $this->assertTrue(method_exists(CategoryController::class, 'index'));
     }
-
-    /**
-     * @dataProvider uriDirections
-     */
-    public function testExist($uri)
-    {
-        $client = static::createClient();
-        $crawler = $client->request('GET', '/map');
-        $this->assertResponseIsSuccessful();
-        $this->assertCount(1, $crawler->filter('.navigation a[href="' . $uri . '"]'));
-    }
 }
